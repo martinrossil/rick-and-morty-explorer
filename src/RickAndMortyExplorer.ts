8492c00155cd5624541c029e532ef636f348bb1d
Martin Rossil <martinrossil@gmail.com>
2021-06-23 10:01:00 +0200

    Created Colors class and swapped references.

--- src/RickAndMortyExplorer.ts
+++ src/RickAndMortyExplorer.ts
@@ -1,12 +1,12 @@
 import { ApplicationElement } from 'enta';
 import AppMachine from './machines/AppMachine';
 import LayoutMachine from './machines/LayoutMachine';
-import Theme from './theme/Theme';
+import Colors from './theme/Colors';
 export default class RickAndMortyExplorer extends ApplicationElement {
     public constructor() {
         super();
         this.name = 'RickAndMortyExplorer';
-        this.backgroundColor = Theme.WARM_GRAY_100;
+        this.backgroundColor = Colors.WARM_GRAY_100;
     }
 
     private appMachine: AppMachine = new AppMachine(this);
