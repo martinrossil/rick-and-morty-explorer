51b28e86c752ba87d18cb9ebb6793149d419d911
Martin Rossil <martinrossil@gmail.com>
2021-06-23 13:41:54 +0200

    Update rollup production.

--- rollup.production.js
+++ rollup.production.js
@@ -30,79 +30,29 @@ export default {
             strip({ include: '**/*.ts' }),
             filesize({ showBrotliSize: true })
         ],
-        output: [
-            {
-                entryFileNames: '[name].' + version + '.es2019.js',
-                dir: './public/',
-                format: 'esm',
-                plugins: [
-                    compiler({
-                        language_in: 'ECMASCRIPT_NEXT',
-                        compilation_level: 'SIMPLE',
-                        language_out: 'ECMASCRIPT_2019'
-                    })
-                ]
-            },
-            {
-                entryFileNames: '[name].' + version + '.es2018.js',
-                dir: './public/',
-                format: 'esm',
-                plugins: [
-                    compiler({
-                        language_in: 'ECMASCRIPT_NEXT',
-                        compilation_level: 'SIMPLE',
-                        language_out: 'ECMASCRIPT_2018'
-                    })
-                ]
-            },
-            {
-                entryFileNames: '[name].' + version + '.es2017.js',
-                dir: './public/',
-                format: 'esm',
-                plugins: [
-                    compiler({
-                        language_in: 'ECMASCRIPT_NEXT',
-                        compilation_level: 'SIMPLE',
-                        language_out: 'ECMASCRIPT_2017'
-                    })
-                ]
-            },
-            {
-                entryFileNames: '[name].' + version + '.es2016.js',
-                dir: './public/',
-                format: 'esm',
-                plugins: [
-                    compiler({
-                        language_in: 'ECMASCRIPT_NEXT',
-                        compilation_level: 'SIMPLE',
-                        language_out: 'ECMASCRIPT_2016'
-                    })
-                ]
-            },
-            {
-                entryFileNames: '[name].' + version + '.es2015.js',
-                dir: './public/',
-                format: 'esm',
-                plugins: [
-                    compiler({
-                        language_in: 'ECMASCRIPT_NEXT',
-                        compilation_level: 'SIMPLE',
-                        language_out: 'ECMASCRIPT_2015'
-                    })
-                ]
-            },
-            {
-                entryFileNames: '[name].' + version + '.es5.js',
-                dir: './public/',
-                inlineDynamicImports: true, // this has to be last, otherwise we get split chunk errors
-                format: 'iife',
-                plugins: [
-                    compiler({
-                        language_in: 'ECMASCRIPT_NEXT',
-                        compilation_level: 'SIMPLE',
-                        language_out: 'ECMASCRIPT5'
-                    })
-                ]
-            }
-        ]
+        output: [ getOutput(2019), getOutput(2018), getOutput(2017), getOutput(2016), getOutput(2015), getOutput(5) ]
     };
+
+function getOutput(year) {
+    const output = {
+        entryFileNames: '[name].' + version + '.es' + year + '.js',
+        dir: './public/',
+        format: 'esm',
+        plugins: [ getCompiler(year) ]
+    }
+    if (year === 5) {
+        output.inlineDynamicImports = true, // this has to be last, otherwise we get split chunk errors
+        output.format = 'iife';
+    }
+    return output;
+}
+
+function getCompiler(year) {
+    let ecma = 'ECMASCRIPT';
+    if (year === 5) {
+        ecma = ecma + year;
+    } else {
+        ecma = ecma + '_' + year;
+    }
+    return compiler({ language_in: 'ECMASCRIPT_NEXT', compilation_level: 'SIMPLE', language_out: ecma })
+}
